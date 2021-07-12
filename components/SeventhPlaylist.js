7d44dd275d984b5806277bc51033b81ff7068e9a
Khoury <khoury.smith@gmail.com>
2021-07-12 12:02:40 -0400

    added user-playlist

--- components/SeventhPlaylist.js
+++ components/SeventhPlaylist.js
@@ -4,13 +4,11 @@ import fetcher from "../middleware/utilities";
 import Playlist from "../components/Playlist";
 
 export default function SeventhPlaylist() {
-  const { data } = useSWR("/api/top-tracks", fetcher);
+  const { data } = useSWR("/api/user-playlist", fetcher);
 
   if (!data) {
     return null;
   }
 
-  return data.playlists.map((playlist, index) => (
-    <Playlist key={playlist.songUrl} {...playlist} />
-  ));
+  return data.playlists.map((playlist, index) => <Playlist {...playlist} />);
 }
